fac02d3f6fad983b47c0b50ef1728ff8087fa0e6
bearystraw <143655276+bearystraw@users.noreply.github.com>
2023-09-11 18:39:25 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,52 +1,8 @@
 import urllib.request as web
 from datetime import datetime
 
-def downloadFile():
+def parse_dates_from_log():
     dl = web.urlopen('https://s3.amazonaws.com/tcmg476/http_access_log')
-    with open('data.txt', 'wb') as file:
+    with open('http_access_log', 'wb') as file:
         file.write(dl.read())
     
-    # Get all IP addresses from the log file
-    with open(http_access_log) as f:
-        log = f.read()
-        regex = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-        ip_list = re.findall(regex, log)
-        return ip_list
-    
-
-log_file = open(filename, "r+")
-for row in log_file:
-    print(row)
-    regex = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-    ip_list = re.findall(regex, row)
-    print()
-    return ip_list
-pass
-
-#trial 3
-parsed_data = []
-  
-with open("http_access_log","r") as file:
-    prev_time = ""
-    data = {}
-    for line in file:
-        time = line.split("[")[1].split("]")[0].split(" ")[0]
-        status_code = line.split('"')[2].split(" ")[1]
-        if prev_time != "":
-            if time == prev_time:
-                data[time]["count"] = data[time]["count"] + 1
-                if status_code in data[time]:
-                    data[time][status_code] = data[time][status_code] + 1
-                else:
-                    data[time][status_code] = 1
-            else:
-                prev_time = time
-                parsed_data.append(data)
-                data = {}
-                data[time] = {"count": 1, status_code: 1}
-        else:
-            prev_time = time
-            data[time] = {"count": 1, status_code: 1}
-
-for i in parsed_data:
-    print(i)
