9a7b182d778e2a51831a6ce18d11991cd989a87e
bearystraw <143655276+bearystraw@users.noreply.github.com>
2023-09-11 18:46:17 -0500

    Update code.py

--- code.py
+++ code.py
@@ -4,3 +4,6 @@ from datetime import datetime
 def parse_dates_from_log():
    with open(LOCAL_FILE, 'r') as f:
     log_content = f.readlines()
+
+pattern = r'\[(\d{2}/\w{3}/\d{4})'
+dates = [re.search(pattern, line).group(1) for
