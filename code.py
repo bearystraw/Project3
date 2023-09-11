c8bb5cb6063ccaaf81c6929898c18109b4844232
bearystraw <143655276+bearystraw@users.noreply.github.com>
2023-09-11 18:42:11 -0500

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,5 @@ import urllib.request as web
 from datetime import datetime
 
 def parse_dates_from_log():
-    dl = web.urlopen('https://s3.amazonaws.com/tcmg476/http_access_log')
-    with open('http_access_log', 'wb') as file:
-        file.write(dl.read())
-    
+   with open(LOCAL_FILE, 'r') as f:
+    log_content = f.readlines()
