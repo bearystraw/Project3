d5f55d460d5d124fa330b475824d292b669dff9d
bearystraw <143655276+bearystraw@users.noreply.github.com>
2023-09-11 18:56:04 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-#we coded together on brandon's computer and split up the typing of the code
+#we worked together on brandon's computer and split up typing the code onto here
 import urllib.request
 import os
 import re
@@ -10,3 +10,13 @@ LOCAL_FILE = "http_access_log.txt"
 def download_log():
    if not os.path.exists(LOCAL_FILE):
       urllib.request.urlretrieve(FILE_URL, LOCAL_FILE)
+
+def parse_dates_from_log():
+   with open(LOCAL_FILE, 'r') as f:
+      log_content = f.readlines()
+
+pattern = r'\[(\d{2}/\w{3}/\d{4})'
+dates = [re.search(pattern, line).group(1) for line in log)content if re.search(pattern, line)]
+
+date_objectives = [datetime.strptime(date, "%d/%b%Y") for date in dates]
+return date_objects
